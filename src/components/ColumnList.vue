5108705575bf88f12fabee871be599bad690b7a2
Di Xiao <mopjtv@gmail.com>
2022-09-05 03:46:29 +0800

    beautify using bootstrap

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -2,35 +2,54 @@
  * @Author: Dee.Xiao
  * @Date: 2022-09-05 02:01:11
  * @LastEditors: Dee.Xiao
- * @LastEditTime: 2022-09-05 02:12:40
+ * @LastEditTime: 2022-09-05 03:38:03
  * @Description: 专栏列表
 -->
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title" />
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border border-light w-25 my-3"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from "vue";
+import { computed } from "vue";
+import noImageIcon from "@/assets/no-image-icon.png";
 
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 
-defineProps({
+const props = defineProps({
   list: {
     type: Array as PropType<ColumnProps[]>,
     required: true,
   },
+});
+
+const columnList = computed(() => {
+  return props.list.map((column) => {
+    if (!column.avatar) {
+      column.avatar = noImageIcon;
+    }
+    return column;
+  });
 });
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
  * @Author: Dee.Xiao
  * @Date: 2022-09-05 01:40:17
  * @LastEditors: Dee.Xiao
- * @LastEditTime: 2022-09-05 02:40:05
+ * @LastEditTime: 2022-09-05 03:30:00
  * @Description: 
 -->
 <script setup lang="ts">
@@ -14,15 +14,25 @@ const list: ColumnProps[] = [
     id: 1,
     title: "test1的专栏",
     description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+    avatar: "https://avatars.githubusercontent.com/u/9313435?v=4",
   },
   {
     id: 2,
     title: "test2的专栏",
     description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+    avatar: "https://avatars.githubusercontent.com/u/9313435?v=4",
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description:
+      "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar: "https://avatars.githubusercontent.com/u/9313435?v=4",
   },
 ];
 </script>
