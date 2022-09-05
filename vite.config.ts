d35ff15621e37d774283991482ae1cc55e424071
Di Xiao <mopjtv@gmail.com>
2022-09-05 16:15:41 +0800

    add element plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
+/*
+ * @Author: Dee.Xiao
+ * @Date: 2022-09-05 01:40:17
+ * @LastEditors: Dee.Xiao
+ * @LastEditTime: 2022-09-05 16:14:27
+ * @Description: 
+ */
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
